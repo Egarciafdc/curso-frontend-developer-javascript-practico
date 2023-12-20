38dedc7f311dc31e16f5682b2522315852709a8e
Ernesto Garcia <egarciafdc@gmail.com>
2023-12-19 23:35:43 -0500

    Agregando menu de hamburguesa y cambiando las media Query

--- script2.js
+++ script2.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const MobileMenu = document.querySelector(".mobile-menu");
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+    MobileMenu.classList.toggle("inactive");
 }
