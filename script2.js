9789889508ee5e789cb03b79976f5324af9a649b
Ernesto Garcia <egarciafdc@gmail.com>
2023-12-21 18:47:25 -0500

    Agregando product detail con css

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector(".menu");
 const menuCarIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
 const MobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container");
 
 
@@ -12,14 +12,14 @@ menuHamIcon.addEventListener("click", toggleMobileMenu);
 menuCarIcon.addEventListener("click", toggleCarAside);
 
 function toggleDesktopMenu() {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
     MobileMenu.classList.add("inactive");
     desktopMenu.classList.toggle("inactive");
 
 }
 
 function toggleMobileMenu() {
-    aside.classList.add("inactive");
+    shoppingCartContainer.classList.add("inactive");
     desktopMenu.classList.add("inactive");
     MobileMenu.classList.toggle("inactive");
 }
@@ -27,7 +27,7 @@ function toggleMobileMenu() {
 function toggleCarAside() {
     desktopMenu.classList.add("inactive");
     MobileMenu.classList.add("inactive");
-    aside.classList.toggle("inactive");
+    shoppingCartContainer.classList.toggle("inactive");
 }
 
 const productList = [];
